aea9c5277adbeeacb5317d7e1aa1f2bd4f99d655
Dylan Richards <dylanerichards@gmail.com>
2019-01-20 17:12:00 -0500

    Add questions to user profile

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -28,6 +28,17 @@ class UserProfile extends Component {
         <div>
           <h2>{this.state.user.first_name} {this.state.user.last_name} <Link to={`/users/${this.props.match.params.id}/edit`}>Edit User</Link></h2>
 
+          <h3>About Me</h3>
+          <h4>{this.state.user.question_1}</h4>
+          <p>{this.state.user.answer_1}</p>
+
+          <h4>{this.state.user.question_2}</h4>
+          <p>{this.state.user.answer_2}</p>
+
+          <h4>{this.state.user.question_3}</h4>
+          <p>{this.state.user.answer_3}</p>
+
+
           <div className="user-images">
           {images}
           </div>
