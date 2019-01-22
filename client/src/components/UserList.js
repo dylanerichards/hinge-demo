28c4c3ef3ad1060d374ec429d284cd65814cd71a
Dylan Richards <dylanerichards@gmail.com>
2019-01-21 23:09:57 -0500

    Update index view

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import axios from "axios"
 import { Link } from "react-router-dom"
+import "../App.css"
+import Card from "@material-ui/core/Card"
+import CardHeader from "@material-ui/core/CardHeader"
+import CardContent from "@material-ui/core/CardContent"
+import Avatar from "@material-ui/core/Avatar"
+import IconButton from "@material-ui/core/IconButton"
+// import MoreVertIcon from "@material-ui/core/MoreVertIcon"
 
 class UserList extends Component {
   constructor(props) {
@@ -23,11 +30,24 @@ class UserList extends Component {
       <div>
       <h1>All Users</h1>
 
+
       { this.state.users.map((user) => {
         return (
-          <div key={user.id}>
-          <Link to={`/users/${user.id}`}> {user.first_name} { user.last_name }</Link>
-          </div>
+          <Link to={`/users/${user.id}`} className="no-decoration">
+            <div key={user.id}>
+              <Card className="user-index-card">
+              <CardHeader
+              avatar={
+                <Avatar>
+                <img src={user.photos[0]} />
+                </Avatar>
+
+              }
+              title={`${user.first_name} ${user.last_name}`}
+              />
+              </Card>
+            </div>
+          </Link>
         )
       })
       }
